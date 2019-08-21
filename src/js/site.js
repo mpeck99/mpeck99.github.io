7f31d60252ddc3891006421fb626149ec6da4a65
Morgan Peck <mlpeck@arkbluecross.com>
2019-08-21 16:08:52 -0500

    adding javascript to check if the location path matches the href on the nav link to add an active class

--- src/js/site.js
+++ src/js/site.js
@@ -4,11 +4,14 @@
 if (document.querySelector('.main-nav')) {
   const path = location.pathname.split('/');
   const href = path[1];
-  // console.log(href);
+  
   const navLink = document.querySelectorAll('.nav-link');
   for(var i=0; i<navLink.length; i++){
     if(navLink[i].getAttribute('href') === href){
        navLink[i].classList.add('active');
+    }
+    else if (href === '' && navLink[i].getAttribute('href') === 'index.html') {
+      navLink[i].classList.add('active');
     }
   }
   
