e4d338209ce0dd19ae8846d3d42505067507693d
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2019-08-21 20:44:49 -0500

    adding a class for transistion on page load

--- src/js/site.js
+++ src/js/site.js
@@ -1,10 +1,21 @@
 // ------------------------------------------------
 // Set active page in nav
 // ------------------------------------------------
+document.addEventListener('DOMContentLoaded', function(){
+  const body = document.querySelector('body');
+
+  if(!body.classList.contains('transition')){
+    body.classList.add('transition');
+  }
+  else {
+    body.classList.remove('transition');
+  }
+});
+
 if (document.querySelector('.main-nav')) {
   const path = location.pathname.split('/');
   const href = path[1];
-  
+
   const navLink = document.querySelectorAll('.nav-link');
   for(var i=0; i<navLink.length; i++){
     if(navLink[i].getAttribute('href') === href){
