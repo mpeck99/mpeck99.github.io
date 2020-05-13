87f8249d7aad6567a406f16bcd82f69d27de90d5
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2020-05-13 15:19:36 -0500

    testing out something

--- src/js/site.js
+++ src/js/site.js
@@ -79,42 +79,39 @@ if(document.querySelector('.hamburger-menu')){
 const movieArray = [];
 const movieWrapper = document.querySelector('.movie-wrapper');
 function personalMovies() {
-    const googleUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1nXRZYrOnedMOvioQJxbIQKTpc9_XCGI23-KMk8jwZQU/values/Movies!A:D?key=AIzaSyDrWhgTrY_NIQ7pa19SpdYtF0Wcom3stDA';
-    fetch(googleUrl).then(response=>{
-        response.json().then(json =>{
-            const movies = json;
-            for(var i = 0; i < movies.values.length; i++){
-                 const apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=e444034c3d7ef62e63059e6e8ac5b828&query='+movies.values[i][0]+'&year='+movies.values[i][1]+'&language=en-US&include_adult=false';
-                fetch(apiUrl).then(response => {
-                    response.json().then(movie => {
-                       for(var i = 0; i <movie.results.length; i++){
-                           let overview  = movie.results[i].overview,
-                                 title = movie.results[i].title,
-                                 year = movie.results[i].release_date.split('-')[0];
-
-                          //  if(movie.results[i].overview.length >= 1000){
-                          //      overview = movie.results[i].overview.substring(0,1000)+'...';
-                          //  }
-                          //  else {
-                               
-                          //  }
-
-                            movieWrapper.innerHTML+= '<div class="card" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.results[i].poster_path+'\');"><div class="details"><h2>'+ title+'</h2><p class="date">'+year+'</p><p class="overview">'+overview+'</p><a href="https://www.themoviedb.org/movie/'+movie.results[i].id+'-'+title+'" target="_blank" class="read-more" aria-label="Read more about'+title+'">Read more</a></div></div>'
-                           break;
-                       } 
-                       
-                    });
-                });
-            } 
-        });
-    });   
+    
 }
 
 if(movieWrapper){
-   personalMovies(); 
+  const googleUrl = 'https://sheets.googleapis.com/v4/spreadsheets/1nXRZYrOnedMOvioQJxbIQKTpc9_XCGI23-KMk8jwZQU/values/Movies!A:D?key=AIzaSyDrWhgTrY_NIQ7pa19SpdYtF0Wcom3stDA';
+  fetch(googleUrl).then(response=>{
+      response.json().then(json =>{
+          const movies = json;
+          for(var i = 0; i < movies.values.length; i++){
+               const apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=e444034c3d7ef62e63059e6e8ac5b828&query='+movies.values[i][0]+'&year='+movies.values[i][1]+'&language=en-US&include_adult=false';
+              fetch(apiUrl).then(response => {
+                  response.json().then(movie => {
+                     for(var i = 0; i <movie.results.length; i++){
+                         let overview  = movie.results[i].overview,
+                               title = movie.results[i].title,
+                               year = movie.results[i].release_date.split('-')[0];
 
-   const movieCard = document.querySelectorAll('.card') ;
-   
+                        //  if(movie.results[i].overview.length >= 1000){
+                        //      overview = movie.results[i].overview.substring(0,1000)+'...';
+                        //  }
+                        //  else {
+                             
+                        //  }
+
+                          movieWrapper.innerHTML+= '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.results[i].poster_path+'\');"><div class="details"><h2>'+ title+'</h2><p class="date">'+year+'</p><p class="overview">'+overview+'</p><a href="https://www.themoviedb.org/movie/'+movie.results[i].id+'-'+title+'" target="_blank" class="read-more" aria-label="Read more about'+title+'">Read more</a></div></div>'
+                         break;
+                     } 
+                     
+                  });
+              });
+          } 
+      });
+  }); 
 }
 
 
