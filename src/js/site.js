739078b726112c1c59803fb848c26d850aca0353
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2020-05-13 22:21:40 -0500

    adding an id for possibly a search input

--- src/js/site.js
+++ src/js/site.js
@@ -119,7 +119,7 @@ setTimeout(function(){
             overview = movie.overview;
         }
         const vote = (movie.vote_average*10);
-        movieWrapper.innerHTML += '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.poster_path+'\');"><div class="details"><h2>'+movie.title+'</h2><span class="score">'+vote+'%</span><p class="tagline">'+movie.tagline+'</p><p class="date">'+movie.release_date.split('-')[0]+'</p><p class="genre">'+movie.genres[0].name+', '+movie.genres[1].name+'</p><p class="overview">'+overview+'</p></div></div>'
+        movieWrapper.innerHTML += '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.poster_path+'\');"><div class="details"><h2 id='+movie.title.replace(/\s+/g, '-')+'>'+movie.title+'</h2><span class="score">'+vote+'%</span><p class="tagline">'+movie.tagline+'</p><p class="date">'+movie.release_date.split('-')[0]+'</p><p class="genre">'+movie.genres[0].name+', '+movie.genres[1].name+'</p><p class="overview">'+overview+'</p></div></div>'
       // break;                     
       }).catch(error => {
         console.error('we came across an error', error);
