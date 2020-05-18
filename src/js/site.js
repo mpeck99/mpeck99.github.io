2e998b9bae8df6173721af723762a3d86c5c8783
Morgan Peck <MorganPeck@Morgan-Pecks-MacBook-Pro.local>
2020-05-17 21:54:28 -0500

    adding the certification to the search results

--- src/js/site.js
+++ src/js/site.js
@@ -101,9 +101,7 @@ function loadMovies(){
               movieWrapper.innerHTML += '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.poster_path+'\');"'+movie.id+'><div class="details"><h2>'+movie.title+'</h2><span class="score"><p>'+vote+'%</p></span><p class="tagline">'+movie.tagline+'</p><ul><li>'+cert+'</li><li>'+movie.release_date.split('-')[0]+'</li><li>'+runtime+'</li><li>'+movie.genres[0].name+'</li></ul><p class="overview">'+overview+'</p></div></div>';
                   }
                 })
-              });
-
-            // break;                     
+          });                    
         }).catch(err => {
           console.error('we came across an error', err);
         })
@@ -153,30 +151,51 @@ if(movieWrapper){
           setTimeout(() => {
               for(var i = 0; i <movie.length; i++){
                 const searchApi = 'https://api.themoviedb.org/3/movie/'+movie[i].id+'?api_key=e444034c3d7ef62e63059e6e8ac5b828&primary_release_year='+movie[i].year+'&year='+movie[i].year+'&query='+movie[i].title.replace(/\s/g, '-')+'&language=en-US';
-
+                const releaseDates = 'https://api.themoviedb.org/3/movie/'+movieArray[i].id+'/release_dates?api_key=e444034c3d7ef62e63059e6e8ac5b828&language=en-US';
                 movieWrapper.innerHTML = '';
 
-                fetch(searchApi).then(response=>{
-                 response.json().then(json=>{
-                   const searchResult = json;
-
-                   if(searchResult.overview.length >= 325){
-                     overview = searchResult.overview.substring(0,325)+'<a href="https://www.themoviedb.org/movie/'+searchResult.id+'-'+searchResult.title+'" target="_blank" class="read-more" aria-label="Read more about'+searchResult.title+'">...read more</a>';
-                   }
-                   else {
-                       overview = searchResult.overview;
-                   }
-         
-                     const vote = (searchResult.vote_average)*10;
-                     const hours = Math.floor(searchResult.runtime / 60);
-                     const mins = searchResult.runtime % 60;
-                     const runtime = hours+'h '+mins+'m';
+                fetch(searchApi).then(response => {
+                  response.json().then(movie => {
+                    fetch(releaseDates).then(res=> {
+                      res.json().then(dates => {
+                        var rating = dates.results.filter(i => i.iso_3166_1 === 'US');
+                        for(var x = 0; x < rating.length; x++){
+                        let overview ='';
+                        if(movie.overview.length >= 325){
+                          overview = movie.overview.substring(0,325)+'<a href="https://www.themoviedb.org/movie/'+movie.id+'-'+movie.title+'" target="_blank" class="read-more" aria-label="Read more about'+movie.title+'">...read more</a>';
+                        }
+                        else {
+                            overview = movie.overview;
+                        }
+                        const vote = (movie.vote_average)*10;
+                        const hours = Math.floor(movie.runtime / 60);
+                        const mins = movie.runtime % 60;
+                        const cert = '';
+                        const runtime = hours+'h '+mins+'m';
                      
-                     movieWrapper.innerHTML += '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+searchResult.poster_path+'\');"id='+searchResult.title.replace(/\s/g, '-').toLowerCase()+'><div class="details"><h2>'+searchResult.title+'</h2><span class="score"><p>'+vote+'%</p></span><p class="tagline">'+searchResult.tagline+'</p><ul><li>'+searchResult.release_date.split('-')[0]+'</li><li>'+runtime+'</li><li>'+searchResult.genres[0].name+'</li></ul><p class="overview">'+overview+'</p></div></div>';
-                 });
-               }).catch(err => {
-                console.error('we came across an error', err);
-              })
+                        if(rating[x].release_dates[0].certification != ""  ){
+                          cert = rating[x].release_dates[0].certification;
+                        }
+                        else if(rating[x].release_dates[1].certification != ""){
+                          cert = rating[x].release_dates[1].certification;
+                        }
+                        else if(rating[x].release_dates[2].certification !=""){
+                          cert = rating[x].release_dates[2].certification;
+                        }
+                        else{
+                          cert = rating[x].release_dates[3].certification;
+                        }
+                        // else  {
+                        //   cert = rating[x].release_dates[0].certification;
+                        // }
+                        movieWrapper.innerHTML += '<div class="card" onclick="this.focus()" style="background-image: url(\'https://image.tmdb.org/t/p/original/'+movie.poster_path+'\');"'+movie.id+'><div class="details"><h2>'+movie.title+'</h2><span class="score"><p>'+vote+'%</p></span><p class="tagline">'+movie.tagline+'</p><ul><li>'+cert+'</li><li>'+movie.release_date.split('-')[0]+'</li><li>'+runtime+'</li><li>'+movie.genres[0].name+'</li></ul><p class="overview">'+overview+'</p></div></div>';
+                            }
+                          })
+                    });                    
+                  }).catch(err => {
+                    console.error('we came across an error', err);
+                  })
+              });
                }
           }, 200);
         }
